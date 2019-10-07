dc34c0442e0e1fbf4ad33cd7a87cef431429d61f
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2019-10-08 02:41:07 +1100

    Update example to make it a little more embed frienly

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ const App: React.FC = () => {
     <Store.Provider><>
       <h1 className='text-2xl text-center m-4'>React hook lazy loading pattern</h1>
       <div className="App mx-auto w-4/5 max-w-md">
-      <p className='my-4'>If you don't know how you got here, check out the blog post or the source code</p>
         <div className='border-black border-2'>
           <div className="flex flex-1">
             <button 
@@ -27,8 +26,12 @@ const App: React.FC = () => {
         </div>
         <h1 className='text-2xl text-center m-4'>👇 Current store contents</h1>
         <ShowStoreContent />
+        <p className='my-4 mt-64'>If you don't know how you got here, check out the 
+          <a className='text-teal-700' href='https://kurthutten.com/blog/react-hook-lazy-loading-pattern/'> blog post</a> or the
+          <a className='text-teal-700' href='https://github.com/Irev-Dev/React-hook-lazy-loading-pattern-demo/tree/master'> source code</a> or the 
+          <a className='text-teal-700' href='https://codesandbox.io/s/github/Irev-Dev/React-hook-lazy-loading-pattern-demo/tree/master'> codesandbox</a>
+        </p>
       </div>
-      
     </></Store.Provider>
   );
 }
